1293559cfba16951a2069512c5299f4c8129697c
hp3icc <49044505+hp3icc@users.noreply.github.com>
2021-12-30 20:51:22 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+cat > /usr/local/bin/auto-reboot1.sh <<- "EOF"
+#!/bin/bash
+sleep 180
+while :
+do
+SERVER=8.8.4.4
+ping -c1 ${SERVER} > /dev/null
+if [ $? != 0 ]
+then
+#
+sudo reboot
+fi
+  sleep 60
+done
+EOF
+#
+cat > /lib/systemd/system/auto-reboot1.service  <<- "EOF"
+[Unit]
+Description=Auto-Reboot1
+
+[Service]
+User=root
+ExecStart=/usr/local/bin/rebooter1.sh
+
+[Install]
+WantedBy=default.target
+
+EOF
+####
+chmod +x /usr/local/bin/auto-reboot1.sh
+chmod 755 /lib/systemd/system/auto-reboot1.service
+systemctl daemon-reload
+systemctl enable autoreboot1.service 
